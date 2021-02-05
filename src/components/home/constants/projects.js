ff83f28a17852485c17b724efc470dfde4f271c9
Andre Araujo <andreearaujo98@gmail.com>
2021-02-05 14:18:49 -0300

    adding one more project

--- src/components/home/constants/projects.js
+++ src/components/home/constants/projects.js
@@ -1,53 +1,68 @@
 const projects = [
-    {
-      name: "Github repositories explorer",
-      projectUrl: "https://github-explorer-delta.vercel.app/",
-      repoUrl: "https://github.com/devaraujstein/github_repositories_explorer",
-      imgPath: "projectMedia/github-explorer.png",
-      imgAlt: "Github Repositories Explorer",
-      summary:
-        "A web app where users can search repositories to get an overview of this one..",
-      keyFeatures: [
-        "Search Repositories",
-        "Data fetching",
-        "Axios, client HTTP",
-        "Design Patterns with Eslint & Prettier",
-      ],
-      technologies: ["ReactJs", "Typescript", "Styled Components", "Github API"],
-    },
-    {
-      name: "Be the hero",
-      projectUrl: "https://bethehero-sigma.vercel.app/",
-      repoUrl: "https://github.com/devaraujstein/SOS11_be_the_hero_frontend",
-      imgPath: "projectMedia/tobehero.png",
-      imgAlt: "Choropleth map preview",
-      summary:
-        "A platform designed for people to find NGOs that need financial help in specific cases.",
-      keyFeatures: [
-        "Data fetching",
-        "Web and mobile platforms",
-        "Optimistic UI",
-        "JWT authentication",
-      ],
-      technologies: ["React", "Javascript", "Nodejs", "MongoDB"],
-    },
-    {
-      name: "Login screen concept",
-      projectUrl: "https://loginscreen-iota.vercel.app/",
-      repoUrl: "https://github.com/devaraujstein/login_screen_nextjs",
-      imgPath: "projectMedia/login_concept.png",
-      imgAlt: "A-note site mocked device preview",
-      summary:
-        "A simple, fast and intuitive login screen inspired by rocketseat.",
-      keyFeatures: [
-        "Declarative UI",
-        "Design System",
-        "Emotion Styled Components",
-        "Login customization",
-      ],
-      technologies: ["React", "Typescript", "NextJS", "Chakra-UI"],
-    },
-  ];
-  
-  export default projects;
-  
+  {
+    name: "Delivery Landing Page",
+    projectUrl: "https://greatdelivery.vercel.app/",
+    repoUrl: "https://github.com/devaraujstein/great-delivery-landing-page-reactjs",
+    imgPath: "projectMedia/great-delivery-lp.png",
+    imgAlt: "Delivery Landing Page",
+    summary:
+      "A landing page made to show a mobile app..",
+    keyFeatures: [
+      "Mobile App",
+      "Ui Concepts",
+      "Multiple Pages",
+      "Landing Page",
+    ],
+    technologies: ["ReactJs", "Javascript", "Material UI"],
+  },
+  {
+    name: "Github repositories explorer",
+    projectUrl: "https://github-explorer-delta.vercel.app/",
+    repoUrl: "https://github.com/devaraujstein/github_repositories_explorer",
+    imgPath: "projectMedia/github-explorer.png",
+    imgAlt: "Github Repositories Explorer",
+    summary:
+      "A web app where users can search repositories to get an overview of this one..",
+    keyFeatures: [
+      "Search Repositories",
+      "Data fetching",
+      "Axios, client HTTP",
+      "Design Patterns with Eslint & Prettier",
+    ],
+    technologies: ["ReactJs", "Typescript", "Styled Components", "Github API"],
+  },
+  {
+    name: "Be the hero",
+    projectUrl: "https://bethehero-sigma.vercel.app/",
+    repoUrl: "https://github.com/devaraujstein/SOS11_be_the_hero_frontend",
+    imgPath: "projectMedia/tobehero.png",
+    imgAlt: "Choropleth map preview",
+    summary:
+      "A platform designed for people to find NGOs that need financial help in specific cases.",
+    keyFeatures: [
+      "Data fetching",
+      "Web and mobile platforms",
+      "Optimistic UI",
+      "JWT authentication",
+    ],
+    technologies: ["React", "Javascript", "Nodejs", "MongoDB"],
+  },
+  {
+    name: "Login screen concept",
+    projectUrl: "https://loginscreen-iota.vercel.app/",
+    repoUrl: "https://github.com/devaraujstein/login_screen_nextjs",
+    imgPath: "projectMedia/login_concept.png",
+    imgAlt: "A-note site mocked device preview",
+    summary:
+      "A simple, fast and intuitive login screen inspired by rocketseat.",
+    keyFeatures: [
+      "Declarative UI",
+      "Design System",
+      "Emotion Styled Components",
+      "Login customization",
+    ],
+    technologies: ["React", "Typescript", "NextJS", "Chakra-UI"],
+  },
+];
+
+export default projects;
